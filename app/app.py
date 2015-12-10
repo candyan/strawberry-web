78ffde63e6202f8b0b319352dc0eb5799a1162c7
Candyan <liuyanhp@gmail.com>
2015-12-10 16:49:35 +0800

    add app_config template

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from views.index_ui import IndexView
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_pyfile('app_config')
 
 app.add_url_rule('/', view_func=IndexView.as_view('index_view'))
 
---
+++ app/utils/store.py
@@ -0,0 +1,4 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
