29a5b23f6df6600354dfa878f8eaee65cf0c4567
Candyan <liuyanhp@gmail.com>
2015-12-31 11:50:29 +0800

    update css

--- app/views/vpn_guides_ui.py
+++ app/views/vpn_guides_ui.py
@@ -3,5 +3,8 @@ from base_ui import BaseView
 
 class VPNGuidesUI(BaseView):
 
+    def template_context(self):
+        return {"current_menu_index": 1}
+
     def template_name(self):
         return "vpn_guides.html"
